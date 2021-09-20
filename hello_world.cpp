124e1f7dcddc0d4324e687a8d968a4ef28ce3004
AmithPrasanth <amithprasanth48@gmail.com>
2021-09-20 22:22:28 +0530

    Changes made

--- hello_world.cpp
+++ hello_world.cpp
@@ -9,6 +9,7 @@ int main()
     cout<<"Enter your name : "<<endl;
     cin>>s;
     cout<<"Welcome to my program "<<s<<endl;
-    cout<<"Creating a branch 1";
+    cout<<"Creating a branch 1"<<endl;
+    cout<<"Making changes after cloning the repository "<<endl;
     return 0;
 }
